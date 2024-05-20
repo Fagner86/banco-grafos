c9b8da8d277088297c8bb4d9eb3164cf605fa7de
Fagner86 <fagnersilva009@gmail.com>
2024-05-20 12:53:50 -0300

    atualizando api

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ function App() {
   const [edges, setEdges] = useState([]);
   const [name, setName] = useState('');
   const [label, setLabel] = useState('');
-  const [selectedNode, setSelectedNode] = useState(null);
-  const [selectedEdge, setSelectedEdge] = useState(null);
   const [showGraph, setShowGraph] = useState(false);
+  const [selectedNodeId, setSelectedNodeId] = useState(null); 
 
   useEffect(() => {
     if (showGraph) {
@@ -50,61 +49,65 @@ function App() {
         console.error('Error creating node:', error);
       });
   };
-
-  const updateNode = () => {
-    if (!selectedNode) return;
-    axios.put(`https://banco-grafos-api.onrender.com/api/nodes/${selectedNode.data.id}`, { name, label })
-      .then(() => {
-        fetchNodes();
-        setName('');
-        setLabel('');
-        setSelectedNode(null);
-      })
-      .catch(error => {
-        console.error('Error updating node:', error);
-      });
+  const editNode = () => {
+    if (selectedNodeId !== null) {
+      const newName = prompt('Digite o novo nome:');
+      const newLabel = prompt('Digite o novo rótulo:');
+  
+      if (newName !== null && newLabel !== null) {
+        // Exclui o nó existente
+        axios.delete(`https://banco-grafos-api.onrender.com/api/nodes/${selectedNodeId}`)
+          .then(() => {
+            // Cria um novo nó com os dados atualizados
+            axios.post('https://banco-grafos-api.onrender.com/api/nodes', { id: selectedNodeId, name: newName, label: newLabel })
+              .then(() => {
+                alert('Nó atualizado com sucesso.');
+                fetchNodes(); // Atualiza a lista de nós
+              })
+              .catch(error => {
+                console.error('Error creating node:', error);
+                alert('Erro ao criar o novo nó. Consulte o console para mais detalhes.');
+              });
+          })
+          .catch(error => {
+            console.error('Error deleting node:', error);
+            alert('Erro ao excluir o nó existente. Consulte o console para mais detalhes.');
+          });
+      }
+    } else {
+      alert('Por favor, selecione um nó para editar.');
+    }
   };
-
-  const deleteNode = () => {
-    if (!selectedNode) return;
-    axios.delete(`https://banco-grafos-api.onrender.com/api/nodes/${selectedNode.data.id}`)
+  
+  
+  
+// Função para excluir um nó
+const deleteNode = () => {
+  // Verifica se há um nó selecionado
+  if (selectedNodeId) {
+    // Envia uma solicitação DELETE para o servidor para excluir o nó no banco de dados
+    axios.delete(`https://banco-grafos-api.onrender.com/api/nodes/${selectedNodeId}`)
       .then(() => {
-        fetchNodes();
-        setSelectedNode(null);
+        const filteredNodes = nodes.filter(node => node.data.id !== selectedNodeId);
+
+        // Remove todas as arestas relacionadas ao nó selecionado
+        const filteredEdges = edges.filter(edge => edge.data.source !== selectedNodeId && edge.data.target !== selectedNodeId);
+    
+        // Atualiza o estado dos nós e das arestas
+        setNodes(filteredNodes);
+        setEdges(filteredEdges);
+    
+        // Limpa o estado do nó selecionado
+        setSelectedNodeId(null);
       })
       .catch(error => {
         console.error('Error deleting node:', error);
+        alert('Erro ao excluir o nó. Consulte o console para mais detalhes.');
       });
-  };
-
-  const deleteEdge = () => {
-    if (!selectedEdge) return;
-    axios.delete(`https://banco-grafos-api.onrender.com/api/edges/${selectedEdge.data.id}`)
-      .then(() => {
-        fetchEdges();
-        setSelectedEdge(null);
-      })
-      .catch(error => {
-        console.error('Error deleting edge:', error);
-      });
-  };
-
-  const handleNodeTap = (event) => {
-    setSelectedNode(event.target);
-    setSelectedEdge(null);
-    setName(event.target.data().name);
-    setLabel(event.target.data().label);
-  };
-
-  const handleEdgeTap = (event) => {
-    setSelectedEdge(event.target);
-    setSelectedNode(null);
-  };
-
-  const handleBackgroundTap = (event) => {
-    setSelectedNode(null);
-    setSelectedEdge(null);
-  };
+  } else {
+    alert('Por favor, selecione um nó para excluir.');
+  }
+};
 
   const handleShowGraph = () => {
     setShowGraph(true);
@@ -113,48 +116,56 @@ function App() {
   const handleAddData = () => {
     setShowGraph(false);
   };
+    // Função para selecionar um nó
+    const selectNode = (nodeId) => {
+      setSelectedNodeId(nodeId);
+    };
 
   return (
     <div>
- <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Graph App</a>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item active">
-          <button className={`nav-link ${!showGraph ? 'active' : ''}`} onClick={handleAddData}>Adicionar Dados</button>
-        </li>
-        <li className="nav-item active">
-          <button className={`nav-link ${showGraph ? 'active' : ''}`} onClick={handleShowGraph}>Mostrar Gráficos</button>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
+      <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+        <div className="container-fluid">
+          <li className="navbar-brand" >Graph App</li>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <button className={`nav-link ${!showGraph ? 'active' : ''}`} onClick={handleAddData}>Adicionar Dados</button>
+              </li>
+              <li className="nav-item active">
+                <button className={`nav-link ${showGraph ? 'active' : ''}`} onClick={handleShowGraph}>Mostrar Gráficos</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
       {showGraph ? (
         <div className="container mt-4">
           <h1 className="mb-2">Gráficos</h1>
+          <>
+              <button className="btn btn-primary me-2" onClick={editNode} disabled={!selectedNodeId}>Editar Nó</button>
+              <button className="btn btn-danger" onClick={deleteNode} disabled={!selectedNodeId}>Excluir Nó</button>
+            </>
           <CytoscapeComponent
             elements={nodes.concat(edges)}
             style={{ width: '100%', height: '500px' }}
             cy={(cy) => {
-              cy.on('tap', 'node', handleNodeTap);
-              cy.on('tap', 'edge', handleEdgeTap);
-              cy.on('tap', handleBackgroundTap);
-              cy.center(); 
+              cy.center();
+              // Adicione um evento de clique para selecionar o nó
+              cy.on('tap', 'node', (event) => {
+                selectNode(event.target.id());
+              });
             }}
           />
         </div>
@@ -167,18 +178,19 @@ function App() {
                 type="text"
                 className="form-control"
                 placeholder="Name"
-                value={name}                onChange={(e) => setName(e.target.value)}
+                value={name} onChange={(e) => setName(e.target.value)}
               />
             </div>
-            <div className="d-flex mb-2 align-items-center">            <div className="input-group mb-2">
-              <input
-                type="text"
-                className="form-control "
-                placeholder="Label"
-                value={label}
-                onChange={(e) => setLabel(e.target.value)}
-              />
-            </div>
+            <div className="d-flex mb-2 align-items-center">
+              <div className="input-group mb-2">
+                <input
+                  type="text"
+                  className="form-control "
+                  placeholder="Label"
+                  value={label}
+                  onChange={(e) => setLabel(e.target.value)}
+                />
+              </div>
             </div>
             <button className="btn btn-primary me-2" onClick={createNode}>Create Node</button>
           </div>
@@ -189,4 +201,3 @@ function App() {
 }
 
 export default App;
-
